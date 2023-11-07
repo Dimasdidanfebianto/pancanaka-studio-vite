03a7d117e8c2b48eb7371ae5c9b00343ff2c23c8
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-11-08 00:07:07 +0700

    update

---
+++ src/components/atom/CenterNav.jsx
@@ -0,0 +1,12 @@
+const CenterNav = () => {
+    return (
+        <div className="flex justify-center items-center mr-20"> 
+            <p className="text-xl mx-5">Home</p>
+            <p className="text-xl mx-5">About Us</p>
+            <p className="text-xl mx-5">Service</p>
+            <p className="text-xl mx-5">Project</p>
+        </div>
+    )
+}
+
+export default CenterNav;
