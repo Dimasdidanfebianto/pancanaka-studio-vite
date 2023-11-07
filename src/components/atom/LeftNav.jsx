177abc40bb6a9b6cb4dcd18e0c31ae7cc78d5400
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-11-08 00:07:12 +0700

    update

---
+++ src/components/atom/LeftNav.jsx
@@ -0,0 +1,10 @@
+function LeftNav() {
+    return (
+        <div className="flex justify-center items-center">
+            <p className="text-3xl font-bold text-orange-500">
+                PancanakaStudio
+            </p>
+        </div>    
+    )
+}
+export default LeftNav;
