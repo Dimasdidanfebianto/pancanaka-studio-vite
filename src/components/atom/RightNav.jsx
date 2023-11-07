e20bad525d2e297934fb84a05f2ee7c7b23cd94b
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-11-08 00:07:16 +0700

    update

---
+++ src/components/atom/RightNav.jsx
@@ -0,0 +1,12 @@
+const RightNav = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <p className="text-xl mx-5">English</p>
+            <div className="flex items-center bg-orange-500 px-5 py-3 rounded-full ml-5">
+                <p className="text-xl text-white">Contact Us</p>
+            </div>
+        </div>
+    )
+}
+
+export default RightNav;
