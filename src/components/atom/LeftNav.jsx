a16b93b717e5d15c53afd8e224aec662e8b029c8
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-11-13 20:43:58 +0700

    update

--- src/components/atom/LeftNav.jsx
+++ src/components/atom/LeftNav.jsx
@@ -1,7 +1,7 @@
 function LeftNav() {
     return (
         <div className="flex justify-center items-center">
-            <p className="text-3xl font-bold text-orange-500">
+            <p className="text-3xl font-navbar-bold text-orange-500">
                 PancanakaStudio
             </p>
         </div>    
