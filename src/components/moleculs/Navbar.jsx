e8c14f07941cd2594c919f35158d51c5707ea300
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-11-13 20:44:19 +0700

    update

--- src/components/moleculs/Navbar.jsx
+++ src/components/moleculs/Navbar.jsx
@@ -4,7 +4,7 @@ import CenterNav from "../atom/CenterNav";
 
 const Navbar = () => {
     return (
-        <div className="flex h-24 bg-black text-white items-center ">
+        <div className="flex h-24 text-white items-center ">
             <div className="flex justify-between w-full px-24">
                 <LeftNav />
                 <CenterNav />
